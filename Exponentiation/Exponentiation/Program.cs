b58d2d451de1b2a1c7be7572d0a234750bb3ed24
Dario Palmisano <Dario.Palmisano@gmail.com>
2019-03-03 14:36:02 +0100

    Exponentiation first draft

---
+++ Exponentiation/Exponentiation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exponentiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the base: ");
+            double number = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the power: ");
+            double power = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(number + " to the " + power + " power equals " +
+                              Math.Pow(number, power));
+
+            Console.ReadLine();
+        }
+    }
+}
