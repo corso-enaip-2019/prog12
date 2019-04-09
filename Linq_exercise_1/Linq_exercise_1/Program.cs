fac52fe5a5d163b3c1e9fa333c043cc9a215c8fc
Dario Palmisano <Dario.Palmisano@gmail.com>
2019-04-09 12:14:59 +0200

    Linq Exercise first draft

--- Linq_exercise_1/Linq_exercise_1/Program.cs
+++ Linq_exercise_1/Linq_exercise_1/Program.cs
@@ -10,20 +10,138 @@ namespace Linq_exercise_1
     {
         static void Main(string[] args)
         {
-            // 1) Numero di inquilini di tutti gli appartamenti di classe enerfgetica A o B
-            // 2) media del numero di inquilini di tutti gli appartamanti
-            // 3) di ogni citta, nome di citta e elenco di vie degli appartamenti
-            // 4) di ogni citta, nome di citta e media di metri quadri
-            // 5) di ogni citta, nome di citta e media di metri quadri
-            //                    tutti ordinati per nome di citta,e dentro le citta ordianate per via.
-            // 6) elenco dei nomi di citta che hano solo appartamentid NON in calsse A, B, C.
+            // 1) numero di inquilini di tutti gli appartamenti di classe energetica A o B
+            // 2) media del numero di inquilini di tutti gli appartamenti
+            // 3) di ogni città, nome di città e elenco di vie degli appartamenti
+            // 4) di ogni città, nome di città e media dei metri quadri
+            // 5) di ogni città, nome di città e media dei metri quadri,
+            //        tutti ordinati per nome di città, e dentro la città ordinati per via.
+            // 6) elenco dei nomi di città che hanno solo appartamenti NON in classe A, B, C.
+
+            IEnumerable<Flat> flats = CreateFlatMocks();
+
+            int sum = 0;
+            foreach (Flat flat in flats.
+                                        Where(f => (f.EnergyClass == EnergyClassType.A ||
+                                                    f.EnergyClass == EnergyClassType.B)))
+            {
+                sum += flat.Flatmates;
+            }
+
+            Console.WriteLine("Numero di inquilini di tutti gli appartamenti di classe energetica A o B: {0}", sum);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            sum = 0;
+            foreach (Flat flat in flats)
+            {
+                sum += flat.Flatmates;
+            }
+
+            Console.WriteLine("Media del numero di inquilini di tutti gli appartamenti: {0}",(double) sum/flats.Count());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Di ogni città, nome di città e elenco di vie degli appartamenti");
+            Console.WriteLine();
+
+            IEnumerable<IGrouping<string, Flat>> flatsByCity = flats.GroupBy(f => f.City);
+            
+            foreach (IGrouping<string, Flat> city in flatsByCity)
+            {
+                Console.WriteLine("Citta': {0}", city.Key);
+
+                foreach (Flat flat in city)
+                {
+                    Console.WriteLine("\tIndirizzo: {0}", flat.Street);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Di ogni città, nome di città e media dei metri quadri");
+            Console.WriteLine();
+
+            foreach (IGrouping<string, Flat> city in flatsByCity)
+            {
+                sum = 0;
+                Console.WriteLine("Citta': {0}", city.Key);
+
+                foreach (Flat flat in city)
+                {
+                    Console.WriteLine("\tIndirizzo: {0}", flat.Street);
+                    sum += flat.SquaresMeters;
+                }
+
+                Console.WriteLine("\tMedia metri quadri: {0}", (double) sum/city.Count());
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Di ogni città, nome di città e media dei metri quadri,");
+            Console.WriteLine("tutti ordinati per nome di città, e dentro la città ordinati per via.");
+            Console.WriteLine();
+
+            IEnumerable<IGrouping<string, Flat>> flatsByCityOrdered = flats
+                .OrderBy(f => f.City)
+                .GroupBy(f => f.City);
+
+            foreach (IGrouping<string, Flat> city in flatsByCityOrdered)
+            {
+                sum = 0;
+                Console.WriteLine("Citta': {0}", city.Key);
+
+                foreach (Flat flat in city.OrderBy(f => f.Street))
+                {
+                    Console.WriteLine("\tIndirizzo: {0}", flat.Street);
+                    sum += flat.SquaresMeters;
+                }
+
+                Console.WriteLine("\tMedia metri quadri: {0}", (double)sum / city.Count());
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Elenco dei nomi di città che hanno solo appartamenti NON in classe A, B, C.");
+
+            foreach (Flat flat in flats.Where(f => f.EnergyClass != EnergyClassType.A &&
+                                                   f.EnergyClass != EnergyClassType.B &&
+                                                   f.EnergyClass != EnergyClassType.C).Distinct(f => f.City))
+            {
+                Console.WriteLine("Citta': {0}", flat.City);
+
+            }
+
+            Console.ReadKey();
         }
+
+        static IEnumerable<Flat> CreateFlatMocks()
+        {
+            return new List<Flat>
+            {
+                new Flat() { SquaresMeters = 50,  Street = "via G. Cesare",          City = "Roma",    Flatmates = 2, EnergyClass = EnergyClassType.A},
+                new Flat() { SquaresMeters = 70,  Street = "via Montenapolene",      City = "Milano",  Flatmates = 1, EnergyClass = EnergyClassType.B},
+                new Flat() { SquaresMeters = 85,  Street = "Largo A. Roiano",        City = "Trieste", Flatmates = 3, EnergyClass = EnergyClassType.F},
+                new Flat() { SquaresMeters = 90,  Street = "via Pestalozzi",         City = "Genova",  Flatmates = 1, EnergyClass = EnergyClassType.E},
+                new Flat() { SquaresMeters = 78,  Street = "Calle S. Andrea",        City = "Venezia", Flatmates = 3, EnergyClass = EnergyClassType.C},
+                new Flat() { SquaresMeters = 65,  Street = "via dei Fori Imperiali", City = "Roma",    Flatmates = 2, EnergyClass = EnergyClassType.C},
+                new Flat() { SquaresMeters = 55,  Street = "via G. Parini",          City = "Milano",  Flatmates = 5, EnergyClass = EnergyClassType.B},
+                new Flat() { SquaresMeters = 100, Street = "via Cavana",             City = "Trieste", Flatmates = 3, EnergyClass = EnergyClassType.E},
+                new Flat() { SquaresMeters = 200, Street = "Piazza Liberta'",        City = "Trieste", Flatmates = 1, EnergyClass = EnergyClassType.F},
+                new Flat() { SquaresMeters = 105, Street = "via Del Porto",          City = "Genova",  Flatmates = 2, EnergyClass = EnergyClassType.G},
+            };
+        }
+
+
     }
     class Flat
     {
         public int SquaresMeters { get; set; }
         public string Street { get; set; }
-        public String City { get; set; }
+        public string City { get; set; }
         public int Flatmates { get; set; }
         public EnergyClassType EnergyClass { get; set; }
     }
---
+++ Test1_Dario_Palmisano/Test1_Dario_Palmisano/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test1_Dario_Palmisano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGui consoleGui = new ConsoleGui();
+
+            LoanApplication loanApplication = new LoanApplication(consoleGui);
+
+            loanApplication.Run();
+        }
+
+        public List<decimal> createInflationMocks()
+        {
+            return new List<decimal>()
+            {
+                1.0m,
+                1.5m,
+                1.7m,
+                1.1m,
+                1.9m,
+                2.5m,
+                3.0m,
+                1.9m,
+                2.2m,
+                3.7m,
+            };
+
+        }
+    }
+
+    public class LoanApplication
+    {
+        bool isFixLoan;
+        bool isClient;
+
+        IGui _gui;
+
+        public LoanApplication(IGui gui)
+        {
+            _gui = gui;
+        }
+
+        public void Run()
+        {
+            bool isFixLoan;
+            bool isClient;
+            ILoanCalculator loanCalculator;
+
+
+            _gui.PrintNL("### Applicazione di calcolo mutuo ###");
+            _gui.PrintNL("");
+
+            decimal loanRequested = _gui.AskForLoan();
+            isFixLoan = _gui.IsFixLoan();
+            isClient = _gui.IsClient();
+
+            if (isFixLoan)
+            {
+                loanCalculator = new FixLoanCalculator();
+            }
+            else
+            {
+                loanCalculator = new VariableLoanCalculator();
+            }
+
+            // Spiacente... non completo!
+
+        }
+    } 
+
+    class FixLoanCalculator : ILoanCalculator
+    {
+        public const decimal FIX_TAX_RATE = 3.5m;
+        public decimal InitialAmount { set; get; }
+        public decimal FinalAmount { set; get; }
+        public bool IsClient { get; set; }
+        public decimal MakeCalculation { get; set; }
+
+        public FixLoanCalculator()
+        {
+            throw new NotImplementedException("Spiacente calcolo non implementato!");
+        }
+    };
+
+    class VariableLoanCalculator : ILoanCalculator
+    {
+        public decimal InitialAmount { get; set; }
+        public decimal FinalAmount { get; set; }
+        public bool IsClient { get; set; }
+        public decimal MakeCalculation { get; set; }
+
+        public VariableLoanCalculator()
+        {
+            throw new NotImplementedException("Spiacente calcolo non implementato!");
+        }
+    };
+
+    interface ILoanCalculator
+    {
+        decimal InitialAmount { set; }
+        decimal FinalAmount { set; }
+        bool    IsClient { set; }
+        decimal MakeCalculation { get; }
+    }
+
+    class ConsoleGui : IGui
+    {
+        public decimal AskForLoan()
+        {
+            decimal loan = 0m;
+
+            Print("Inserire il valore del mutuo richiesto: ");
+
+            do
+            {
+                if (!decimal.TryParse(Console.ReadLine(), out loan) || loan <= 0m)
+                {
+                    PrintNL("Il valore del mutuo deve essere un numero > 0. Il valore inserito non e' valido!");
+                    PrintNL("");
+                }
+            }
+            while (loan <= 0m);
+            PrintNL("");
+
+            return loan;
+        }
+
+        public string AskForString(string message)
+        {
+            string value;
+
+            Print(message);
+
+            value = Console.ReadLine();
+
+            return value;
+        }
+
+        public void Print(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void PrintNL(string message)
+        {
+            Print(message + Environment.NewLine);
+        }
+
+        public bool IsFixLoan()
+        {
+            string isValidString;
+
+            PrintNL("Se si richiede un mutuo a tasso fisso (specificare f)");
+            PrintNL("Se si richiede un mutuo a tasso variabile (specificare v)");
+            PrintNL("");
+
+            do
+            {
+                isValidString = AskForString("> ").ToLower();
+
+                if (isValidString != "f" && isValidString != "v")
+                {
+                    PrintNL("IL valore inserito non e' valido (diverso da f e v). Reinserire");
+                    PrintNL("");
+                }
+            }
+            while (isValidString != "f" && isValidString != "v");
+            PrintNL("");
+
+            return (isValidString == "f") ? true : false;
+        }
+
+        public bool IsClient()
+        {
+            string isValidString;
+
+            PrintNL("Si e' clienti della banca (si/no)?");
+
+            do
+            {
+                isValidString = AskForString("> ").ToLower();
+
+                if (isValidString != "si" && isValidString != "no")
+                {
+                    PrintNL("IL valore inserito non e' valido (diverso da si e no). Reinserire");
+                    PrintNL("");
+                }
+            }
+            while (isValidString != "si" && isValidString != "no");
+            PrintNL("");
+
+            return (isValidString == "si") ? true : false;
+        }
+    }
+
+    public interface IGui
+    {
+        decimal AskForLoan();
+        void PrintNL(string message);
+        bool IsFixLoan();
+        bool IsClient();
+    }
+}
