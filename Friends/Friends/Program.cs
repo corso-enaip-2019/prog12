d5ec6f633442b0e56bdfe9d86c3548b2db28cff9
Dario Palmisano <Dario.Palmisano@gmail.com>
2019-04-16 16:14:08 +0200

    NotifyingList first draft

---
+++ Friends/Friends/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kid dario = new Kid("Dario");
+            Kid vito = new Kid("Vito");
+            Kid antonio = new Kid("Antonio");
+            Kid carillo = new Kid("carillo");
+
+            dario.AddFriend(vito);
+
+            Console.WriteLine("Amici di Dario");
+
+            foreach (Kid kid in dario.Friends)
+            {
+                Console.WriteLine(kid.Name);
+            }
+
+            Console.WriteLine("Amici di Vito");
+
+            foreach (Kid kid in vito.Friends)
+            {
+                Console.WriteLine(kid.Name);
+            }
+
+            dario.AddFriend(antonio);
+            Console.WriteLine("Amici di Dario");
+
+            foreach (Kid kid in dario.Friends)
+            {
+                Console.WriteLine(kid.Name);
+            }
+
+            Console.WriteLine("Amici di Antonio");
+
+            foreach (Kid kid in antonio.Friends)
+            {
+                Console.WriteLine(kid.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Kid
+    {
+        public Kid(string name)
+        {
+            Name = name;
+            _Friend = new List<Kid>();
+        }
+
+        public string Name { get; }
+
+        public void AddFriend(Kid kid)
+        {
+            if (! _Friend.Contains(kid))
+            {
+                _Friend.Add(kid);
+            }
+
+            if (!kid.Friends.Contains(this))
+            {
+                kid.AddFriend(this);
+            }
+        }
+
+        public List<Kid> Friends
+        {
+            get
+            {
+                return _Friend.ToList();
+            }
+        }
+
+        private List<Kid> _Friend;
+    }
+}
---
+++ NotifyingList/NotifyingList/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotifyingList<string> nl = new NotifyingList<string>();
+
+            nl.OnListOperation += Message;
+
+            nl.Add(null);
+            nl.Add("Dario");
+            nl.Add("Francoise");
+            nl.Add("Filippo");
+
+            nl.Remove(null);
+            nl.Remove("Filippo");
+
+            Console.ReadKey();
+        }
+
+        static public void Message(object o, EventArgsOperationAndArg e)
+        {
+            Console.WriteLine("Operation on List op: {0}, arg: {1}", e.Operation, e.Arg);
+        }
+    }
+
+    class NotifyingList<T>
+    {
+        List<T> _list;
+
+        public NotifyingList()
+        {
+            _list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            if (item != null)
+            {
+                _list.Add(item);
+
+                if (OnListOperation != null)
+                {
+                    OnListOperation.Invoke(this, new EventArgsOperationAndArg("Add", item.ToString()));
+                }
+            }
+        }
+
+        public void Remove(T item)
+        {
+            if (item != null && _list.Contains(item))
+            {
+                _list.Remove(item);
+
+                if (OnListOperation != null)
+                {
+                    OnListOperation.Invoke(this, new EventArgsOperationAndArg("Remove", item.ToString()));
+                }
+            }
+        }
+
+        public event ListOperationEvent OnListOperation;
+    }
+
+    public delegate void ListOperationEvent(object o, EventArgsOperationAndArg e);
+
+    public class EventArgsOperationAndArg : EventArgs
+    {
+        public EventArgsOperationAndArg(string op, string arg)
+        {
+            Operation = op;
+            Arg = arg;
+        }
+
+        public string Operation { get; }
+        public string Arg { get; }
+    }
+}
